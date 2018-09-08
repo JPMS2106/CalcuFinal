fcb6b82221230dc43509cb502952342c42bd78d9
Pablo mendez <jpms210693@hotmail.es>
2018-09-07 23:06:40 -0600

    tome

--- CalcuFinal/CalcuFinal/main.cpp
+++ CalcuFinal/CalcuFinal/main.cpp
@@ -1,18 +1,20 @@
 #include"Lista.h"
-
+#include<string>
 void main() {
 	Lista *l = new Lista();
 	Pila *pila = new Pila();
 	Cola *cola = new Cola();
+	string salida;
 	//Pila*pila2 = new Pila();
 	cout << "Imprimiendo Lista" << endl;
 	l->recorrer("-1249");
 	l->buscarOperator(pila,cola);
-	l->imprimirLista();
+	l->imprimirLista()<<endl;
 	//pila2 = pila->restructurar(pila);
 	cout << "Imprimiendo Pila" << endl;
 	while (pila->getTam() != 0) {
-		cout << pila->pop() << endl;;
+		salida = pila->pop();
+		cout <<salida<<endl;
 	}
 	cout << "Imprimiendo cola" << endl;
 	cout<<cola->dequeue();
